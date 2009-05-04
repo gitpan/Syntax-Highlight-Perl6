2c522f365d26d9172cadb2aa8d9e32a30ecf1754
أحمد محمد زواوي - Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2009-05-04 12:59:31 +0000

    Import of AZAWAWI/Syntax-Highlight-Perl6-0.50 from CPAN.
    gitpan-cpan-distribution: Syntax-Highlight-Perl6
    gitpan-cpan-version:      0.50
    gitpan-cpan-path:         AZAWAWI/Syntax-Highlight-Perl6-0.50.tar.gz
    gitpan-cpan-author:       AZAWAWI
    gitpan-cpan-maturity:     released

--- lib/Syntax/Highlight/Perl6.pm
+++ lib/Syntax/Highlight/Perl6.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Perl6.pm 26571 2009-04-30 01:30:27Z azawawi $
+# $Id: Perl6.pm 26593 2009-05-01 09:20:11Z azawawi $
 #
 package Syntax::Highlight::Perl6;
 
@@ -24,7 +24,7 @@ use STD;
 
 # exports and version
 our @EXPORT_OK = qw();
-our $VERSION = '0.49';
+our $VERSION = '0.50';
 
 # filename constants
 Readonly my $FILE_CSS    => 'p6_style.css';
